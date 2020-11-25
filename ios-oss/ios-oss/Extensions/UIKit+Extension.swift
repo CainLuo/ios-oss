0f0b883da8970ec6fd5c7cccfce46dc95ea2bba7
CainLuo <350116542@qq.com>
2020-11-25 12:05:01 +0800

    Update UIKit+Extension.swift

--- ios-oss/ios-oss/Extensions/UIKit+Extension.swift
+++ ios-oss/ios-oss/Extensions/UIKit+Extension.swift
@@ -50,10 +50,6 @@ extension UIView {
         }
     }
 
-    class func fromNib<T: UIView>() -> T {
-        return Bundle.main.loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
-    }
-
     func cornerView(_ corners: UIRectCorner, size: CGSize) {
         let mark = CAShapeLayer()
         let bezierPath = UIBezierPath(roundedRect: bounds,
