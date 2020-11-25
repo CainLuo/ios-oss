7b66a3151c16d1df0ef21cd01dd371b711c674d4
CainLuo <350116542@qq.com>
2020-11-25 12:04:45 +0800

    Update UIColor+Extension.swift

--- ios-oss/ios-oss/Extensions/UIColor+Extension.swift
+++ ios-oss/ios-oss/Extensions/UIColor+Extension.swift
@@ -9,12 +9,8 @@ import UIKit
 
 extension UIColor {
     
-    // MARK: - Methods
     /// Hex sRGB color initializer.
-    ///
-    /// - parameter hex: Pass in a sRGB color integer using hex notation, i.e. 0xFFFFFF. Make sure to only include 6 hex digits.
-    ///
-    /// - returns: Initialized opaque UIColor, i.e. alpha is set to 1.0.
+    /// - Parameter hex: Pass in a sRGB color integer using hex notation, i.e. 0xFFFFFF. Make sure to only include 6 hex digits.
     public convenience init(_ hex: Int) {
         assert(
             0...0xFFFFFF ~= hex,
@@ -25,23 +21,22 @@ extension UIColor {
         let blue = (hex & 0x0000FF)
         self.init(red: red, green: green, blue: blue)
     }
-    
+
     /// RGB integer color initializer.
-    ///
-    /// - parameter red:   Red component as integer. In iOS 9 or below, this value should be between 0 and 255. iOS 10
+    /// - Parameters:
+    ///   - red: Red component as integer. In iOS 9 or below, this value should be between 0 and 255. iOS 10
     ///                    and above uses an extended color space to support wide color.
-    /// - parameter green: Green component as integer. In iOS 9 or below, this value should be between 0 and 255. iOS 10
+    ///   - green: Green component as integer. In iOS 9 or below, this value should be between 0 and 255. iOS 10
     ///                    and above uses an extended color space to support wide color.
-    /// - parameter blue:  Blue component as integer. In iOS 9 or below, this value should be between 0 and 255. iOS 10
+    ///   - blue: Blue component as integer. In iOS 9 or below, this value should be between 0 and 255. iOS 10
     ///                    and above uses an extended color space to support wide color.
-    ///
-    /// - returns: Initialized opaque UIColor, i.e. alpha is set to 1.0.
-    public convenience init(red: Int, green: Int, blue: Int) {
+    ///   - alpha: Color alpha, Default 1.0
+    public convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
         self.init(
             red: CGFloat(red) / 255.0,
             green: CGFloat(green) / 255.0,
             blue: CGFloat(blue) / 255.0,
-            alpha:  1.0
+            alpha:  alpha
         )
     }
 }
