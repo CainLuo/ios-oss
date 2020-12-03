9b34159a4bdfd05df89afc5e7ea47d686ff76182
Lbs <627720876@qq.com>
2020-12-03 11:21:26 +0800

    添加导航栏适配方案

---
+++ ios-oss/ios-oss/Base/BaseNavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  BaseNavigationBar.swift
+//  ios-oss
+//
+//  Created by YYKJ0046 on 2020/12/3.
+//
+
+import UIKit
+import HBDNavigationBar
+
+class BaseNavigationBar: HBDNavigationBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
